6eca6801eda511eb55eccb8ccd2a68188ff22c7a
Pixzzz <eleazar1225@hotmail.com>
2024-06-28 21:40:06 -0400

    Registro usuarios

---
+++ Mini-MarketX.Web/Controllers/AccountController1.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Mini_MarketX.Web.Models;
+
+
+namespace Mini_MarketX.Web.Controllers
+{
+    public class AccountController1 : Controller
+    {
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+               
+                TempData["SuccessMessage"] = "Registro exitoso";
+                return RedirectToAction("Index", "Home");
+            }
+
+            return View(model);
+        }
+    }
+}
