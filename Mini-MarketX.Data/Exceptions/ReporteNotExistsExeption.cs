2109560b48915a5353df69929e7522a15a3cd29d
Yanser Nuñez <yansernunez9@gmail.com>
2024-06-30 17:42:04 -0400

    Actualizacion de los archivos del requerimiento de reportes de inventario

---
+++ Mini-MarketX.Data/Exceptions/ReporteNotExistsExeption.cs
@@ -0,0 +1,10 @@
+﻿namespace Mini_MarketX.Data.Exceptions
+{
+    public class ReporteNotExistsExeption : Exception
+    {
+        public ReporteNotExistsExeption(string message) : base(message)
+        {
+            // x logica para guardar el log del error y enviar la notificacion.
+        }
+    }
+}
---
+++ Mini-MarketX.Data/Interfaces/IReporteRepository.cs
@@ -0,0 +1,13 @@
+﻿using Mini_MarketX.Data.Entities;
+
+namespace Mini_MarketX.Data.Interfaces
+{
+    public interface IReporteRepository
+    {
+        void Agregar(Reporte reporte);
+        void Actualizar(Reporte reporte);
+        void Remover(Reporte reporte);
+        List<Reporte> TraerTodos();
+        Reporte ObtenerPorId(int reporteId);
+    }
+}
