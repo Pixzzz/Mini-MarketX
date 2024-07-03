30ed1169cfe5f8edcea0fc71b5feddba3f60a9c8
Pixzzz <eleazar1225@hotmail.com>
2024-07-03 11:07:40 -0400

    data cambios

---
+++ Mini-MarketX.Data/Context/Mini_MarketXContext.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Mini_MarketX.Data.Entities;
+
+namespace Mini_MarketX.Data.Context
+{
+    public class Mini_MarketXContext : DbContext
+    {
+
+        public Mini_MarketXContext(DbContextOptions<Mini_MarketXContext> options) : base(options)
+        {
+
+        }
+
+
+
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    optionsBuilder.UseInMemoryDatabase("MiniMarketXDB");
+        //    base.OnConfiguring(optionsBuilder);
+        //}
+
+        //public DbSet<Reporte>? Reportes { get; set; }
+        //public DbSet<Producto>? Productos { get; set; }
+        public DbSet<User> Users { get; set; }
+    }
+}
